d9821376dbdf16f4289729745383b940b56c36c5
joncos <joncosrep@gmail.com>
2013-10-26 05:16:39 +0300

    Add currency library as class using module.exports

--- currency_run.js
+++ currency_run.js
@@ -1,4 +1,11 @@
-var currency = require('./lib/currency');
+// var currency = require('./lib/currency');
 
-console.log('50 Canadian$ are ' + currency.canadianToUS(50) + ' US$');
+// console.log('50 Canadian$ are ' + currency.canadianToUS(50) + ' US$');
+// console.log('50 US$ are ' + currency.usToCanadian(50) + ' Canadian$');
+
+var Currency = require('./lib/currency_as_class'),
+    parity = 0.91;
+
+currency = new Currency(parity);
+console.log('50 Canadian$ are ' + currency.canadianToUs(50) + ' US$');
 console.log('50 US$ are ' + currency.usToCanadian(50) + ' Canadian$');
